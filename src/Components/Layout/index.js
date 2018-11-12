8815416fe28efed784ee4267d81b61a8b1035ee2
Unknown <johan.gudmundsson2012@gmail.com>
2018-11-12 12:43:57 +0100

    Add exports

---
+++ src/Components/Layout/index.js
@@ -0,0 +1,4 @@
+export { default as FlexBox } from './FlexBox';
+export { default as Layout } from './Layout';
+export { default as Modal } from './Modal';
+export { default as Wrapper } from './Wrapper';
