0b72ed8300228c8a619c55d625f1d4f60f09afaf
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-11-16 11:42:58 +0100

    Clean up styles
    Moved styles into the css method from styled components v4 to avoid duplicated styles, also fixed broken spin component after upgrading to v4.

--- src/HoC/Spin/index.js
+++ src/HoC/Spin/index.js
@@ -1,15 +1,19 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 const spin = keyframes`
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
 `;
 
+const animation = css`
+  ${spin} 1s linear infinite
+`;
+
 const Spin = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  animation: ${props => (props.animate ? `${spin} 1s linear infinite` : null)};
+  animation: ${props => props.animate && animation};
 `;
 
 export default Spin;
--- src/components/Elements/Title/Headings.js
+++ src/components/Elements/Title/Headings.js
@@ -1,77 +1,47 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import defaultTheme from '../../../constants/defaultTheme';
 
+const baseStyle = css`
+  text-transform: ${props => (props.capitalize ? 'capitalize' : 'none')};
+  text-align: ${props => (props.center ? 'center' : 'left')};
+  color: ${props =>
+    props.secondary ? props.theme.black30 : props.theme.primary};
+  font-weight: ${props => (props.weight ? props.weight : 'normal')};
+  font-family: ${props =>
+    props.secondary
+      ? "'Encode Sans', sans-serif"
+      : 'Arial,Helvetica,sans-serif'};
+`;
+
 export const Heading1 = styled.h1`
+  ${baseStyle}
   margin: ${props => (props.margin ? props.margin : '.67em 0')};
   font-size: ${props => (props.fontSize ? props.fontSize : '2em')};
-  text-transform: ${props => (props.capitalize ? 'capitalize' : 'none')};
-  text-align: ${props => (props.center ? 'center' : 'left')};
-  color: ${props => (props.secondary ? props.theme.black30 : '#001529')};
-  font-weight: ${props => (props.weight ? props.weight : 'normal')};
-  font-family: ${props =>
-    props.secondary
-      ? "'Encode Sans', sans-serif"
-      : 'Arial,Helvetica,sans-serif'};
 `;
 export const Heading2 = styled.h2`
+  ${baseStyle}
   margin: ${props => (props.margin ? props.margin : '.75em 0')};
   font-size: ${props => (props.fontSize ? props.fontSize : '1.5em')};
-  text-transform: ${props => (props.capitalize ? 'capitalize' : 'none')};
-  text-align: ${props => (props.center ? 'center' : 'left')};
-  color: ${props => (props.secondary ? props.theme.black30 : '#001529')};
-  font-weight: ${props => (props.weight ? props.weight : 'normal')};
-  font-family: ${props =>
-    props.secondary
-      ? "'Encode Sans', sans-serif"
-      : 'Arial,Helvetica,sans-serif'};
 `;
 export const Heading3 = styled.h3`
+  ${baseStyle}
   margin: ${props => (props.margin ? props.margin : '.83em 0')};
   font-size: ${props => (props.fontSize ? props.fontSize : '1.17em')};
-  text-transform: ${props => (props.capitalize ? 'capitalize' : 'none')};
-  text-align: ${props => (props.center ? 'center' : 'left')};
-  color: ${props => (props.secondary ? props.theme.black30 : '#001529')};
-  font-weight: ${props => (props.weight ? props.weight : 'normal')};
-  font-family: ${props =>
-    props.secondary
-      ? "'Encode Sans', sans-serif"
-      : 'Arial,Helvetica,sans-serif'};
 `;
 export const Heading4 = styled.h4`
+  ${baseStyle}
   margin: ${props => (props.margin ? props.margin : '1.12em 0')};
   font-size: ${props => (props.fontSize ? props.fontSize : '1em')};
-  text-transform: ${props => (props.capitalize ? 'capitalize' : 'none')};
-  text-align: ${props => (props.center ? 'center' : 'left')};
-  color: ${props => (props.secondary ? props.theme.black30 : '#001529')};
-  font-weight: ${props => (props.weight ? props.weight : 'normal')};
-  font-family: ${props =>
-    props.secondary
-      ? "'Encode Sans', sans-serif"
-      : 'Arial,Helvetica,sans-serif'};
 `;
 export const Heading5 = styled.h5`
+  ${baseStyle}
   margin: ${props => (props.margin ? props.margin : '1.5em 0')};
   font-size: ${props => (props.fontSize ? props.fontSize : '.83em')};
-  text-transform: ${props => (props.capitalize ? 'capitalize' : 'none')};
-  text-align: ${props => (props.center ? 'center' : 'left')};
-  color: ${props => (props.secondary ? props.theme.black30 : '#001529')};
-  font-weight: ${props => (props.weight ? props.weight : 'normal')};
-  font-family: ${props =>
-    props.secondary
-      ? "'Encode Sans', sans-serif"
-      : 'Arial,Helvetica,sans-serif'};
 `;
 export const Heading6 = styled.h6`
+  ${baseStyle}
   margin: ${props => (props.margin ? props.margin : '1.67em 0')};
   font-size: ${props => (props.fontSize ? props.fontSize : '.75em')};
-  text-transform: ${props => (props.capitalize ? 'capitalize' : 'none')};
-  text-align: ${props => (props.center ? 'center' : 'left')};
-  color: ${props => (props.secondary ? props.theme.black30 : '#001529')};
-  font-weight: ${props => (props.weight ? props.weight : 'normal')};
-  font-family: ${props =>
-    props.secondary
-      ? "'Encode Sans', sans-serif"
-      : 'Arial,Helvetica,sans-serif'};
 `;
 
 Heading1.defaultProps = defaultTheme;
