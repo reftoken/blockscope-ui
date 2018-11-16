adc4e7c25a40befd331913fd3999e7cb0db55fa0
Unknown <johan.gudmundsson2012@gmail.com>
2018-11-16 08:05:39 +0100

    Add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs'
+  },
+  external: [
+    'polished',
+    'react',
+    'react-dom',
+    'styled-components',
+    'prop-types'
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    uglify()
+  ]
+};
